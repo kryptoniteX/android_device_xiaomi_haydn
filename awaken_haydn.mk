ad69ccdb8fffc90fb1af0253bfbb5409474eea11
Puneet Gautam <punitrajgautam@gmail.com>
2023-01-27 18:31:07 +0530

    umm

---
+++ awaken_haydn.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from haydn device
+$(call inherit-product, device/xiaomi/haydn/device.mk)
+
+# Inherit some common AwakenOS stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
+USE_PIXEL_CHARGER := true
+USE_GAPPS := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := awaken_haydn
+PRODUCT_DEVICE := haydn
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := M2012K11I
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
---
+++ vendorsetup.sh
@@ -0,0 +1,23 @@
+echo 'Starting to clone stuffs needed to build for Haydn'
+
+# Kernel
+echo 'Cloning kernel tree [1/5]'
+git clone --depth=1 https://github.com/Alucard-Storm/kernel_xiaomi_haydn.git -b twelve kernel/xiaomi/haydn
+
+# Vendor
+echo 'Cloning vendor tree [2/5]'
+git clone --depth=1 https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn.git -b arrow13 vendor/xiaomi/haydn
+
+# Firmware
+echo 'Cloning firmware [3/5]'
+git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn-firmware.git -b thirteen vendor/xiaomi/haydn-firmware
+
+# Camera
+echo 'Cloning MIUI Camera [4/5]'
+git clone https://gitlab.com/Alucard_Storm/haydn-miuicamera -b twelve vendor/xiaomi/haydn-miuicamera
+
+# Logging
+echo 'Cloning system logging [5/5]'
+rm -rf system/logging && git clone https://github.com/Astridxx/system_logging -b tiramisu system/logging
+
+echo 'delete vendorsetup.sh from device tree once this is done'
